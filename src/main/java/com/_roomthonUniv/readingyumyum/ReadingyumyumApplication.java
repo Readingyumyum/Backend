6f732f4edbe20dfabf11e0ee686c90f615fcc2b2
MJJ <m3k0813@gmail.com>
2024-10-28 18:02:08 +0900

    chore: Spring Boot 초기세팅

---
+++ src/main/java/com/_roomthonUniv/readingyumyum/ReadingyumyumApplication.java
@@ -0,0 +1,13 @@
+package com._roomthonUniv.readingyumyum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingyumyumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadingyumyumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/_roomthonUniv/readingyumyum/ReadingyumyumApplicationTests.java
@@ -0,0 +1,13 @@
+package com._roomthonUniv.readingyumyum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingyumyumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
